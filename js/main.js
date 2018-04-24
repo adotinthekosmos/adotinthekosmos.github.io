ffdabee6f9841afe87932dfeccab21714761ab29
khoapa <khoateamer@gmail.com>
2018-04-24 23:46:30 +0700

    Add About me section + bigAssImg1 section and fix minor padding, margin

--- js/main.js
+++ js/main.js
@@ -203,3 +203,16 @@ function showPage() {
     document.getElementById("hidden-wrapper").style.display = "block"
     new WOW().init();
 }
+
+$('.js-tilt').tilt({
+    maxTilt: 20,
+    perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+    easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+    scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+    speed: 300, // Speed of the enter/exit transition.
+    transition: true, // Set a transition on enter/exit.
+    disableAxis: null, // What axis should be disabled. Can be X or Y.
+    reset: true, // If the tilt effect has to be reset on exit.
+    glare: true, // Enables glare effect
+    maxGlare: 1 // From 0 - 1.
+})
